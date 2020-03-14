8a2b634f9b4256c18701361bb9ca80f5950041c5
trannamtrung1st <trannamtrung1st@gmail.com>
2020-03-15 00:28:30 +0700

    gio moi finish hihi <3

--- WorkManager/src/api/NotiApi.js
+++ WorkManager/src/api/NotiApi.js
@@ -1,4 +1,5 @@
 function handleNotification(notification) {
+  if (!notification) return;
   console.log("NOTIFICATION:", notification);
   alert("Go to the entity specified in message to see the detail information");
 }
